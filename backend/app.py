e8c819e0027b14f57bbb9f15f6b917f98a7bf895
abhinav112 <nabhinav2000@gmail.com>
2022-02-27 03:46:55 +0800

    api integration

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request,send_file
 from flask_cors import CORS
 import os
 from PIL import Image
@@ -7,6 +7,10 @@ import requests
 from pipeline.openpose import open_pose
 from pipeline.human_parser import human_parser
 from pipeline.clothe_mask import generate_clothe_mask
+from binascii import a2b_base64
+from datauri import DataURI
+
+
 
 # Flask Config
 app = Flask(__name__)
@@ -20,12 +24,15 @@ UPLOAD_PATH = f"{os.getcwd()}/data"
 
 @app.route("/api", methods=["POST"])
 def endpoint():
-    req_file = request.files["human"]
+    req_file = request.form["human"]
+    uri = DataURI(req_file)
+    print(uri.mimetype)
+    human_filepath = f"{UPLOAD_PATH}/human.jpg"
+    with open(human_filepath, "wb") as f:
+        f.write(uri.data)
+
     cloth_url = request.form["clotheUrl"]
     clothe_path = f"{UPLOAD_PATH}/cloth_img.jpg"
-    human_filename = req_file.filename
-    human_filepath = f"{UPLOAD_PATH}/{human_filename}"
-    req_file.save(human_filepath) 
     response = requests.get(cloth_url)
     with open(clothe_path, "wb") as f:
         f.write(response.content)
@@ -33,8 +40,8 @@ def endpoint():
     open_pose_json = open_pose(human_filepath)
     human_parser(human_filepath)
     generate_clothe_mask(clothe_path)
-    print(open_pose_json)
-    return json.dumps(open_pose_json)
+
+    return send_file(human_filepath)
 
 if __name__ == "__main__":
     if(not os.path.isdir(UPLOAD_PATH)):
